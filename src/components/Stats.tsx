01ddeda5d278728b61e9f1f17d0da28f201aaf52
raisingangel82 <gianlucaborrelli.gb@gmail.com>
2025-07-03 16:09:40 +0200

    remove swiper

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -8,8 +8,6 @@ import { getColor } from '../data/colorPalette';
 import { useBreakpoint } from '../hooks/useBreakpoint';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
 
 interface StatsProps {
     posts: Post[];
