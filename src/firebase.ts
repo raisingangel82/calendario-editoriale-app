fa6d29f2b5b0dcbf655e62b57b7d2c0089e93b2b
Gianluca Borrelli <gianlucaborrelli.gb@gmail.com>
2025-07-05 14:35:48 +0200

    UI refinement 5

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  apiKey: "AIzaSyA3EiyPqxAJRo6A8xnva0iB3R2XGZnKPWI",
+  authDomain: "calendario-editoriale-so-bc85b.firebaseapp.com",
+  projectId: "calendario-editoriale-so-bc85b",
+  storageBucket: "calendario-editoriale-so-bc85b.firebasestorage.app",
+  messagingSenderId: "163208289900",
+  appId: "1:163208289900:web:ac529fa7abd47cb64b4249",
+  measurementId: "G-386XG3W2CE"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
